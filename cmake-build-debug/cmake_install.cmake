8a52a0dc46868148255cfd32a3f34c70f9e598fa
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-03-13 14:38:13 +0000

    Changed the CMakeLists.txt according to the termination of the actual folder and files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/gabrielademattosbarbozadasilva/Documents/Documentos/FEUP/2ANO/2Semestre/DA/Proj1/DA2425_PRJ1_G<TN>
+# Install script for directory: /home/francisca/uni/2ano/2S/DA/DA2425_PROJ1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -34,7 +39,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -52,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/gabrielademattosbarbozadasilva/Documents/Documentos/FEUP/2ANO/2Semestre/DA/Proj1/DA2425_PRJ1_G<TN>/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
