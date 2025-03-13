7ab5033bbfb12523760f2e8dbcb9bc15935c6fae
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-03-13 14:38:13 +0000

    Changed the CMakeLists.txt according to the termination of the actual folder and files

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/SetUp/Input.cpp" "CMakeFiles/untitled.dir/SetUp/Input.cpp.o" "gcc" "CMakeFiles/untitled.dir/SetUp/Input.cpp.o.d"
+  "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/SetUp/Main.cpp" "CMakeFiles/untitled.dir/SetUp/Main.cpp.o" "gcc" "CMakeFiles/untitled.dir/SetUp/Main.cpp.o.d"
+  "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/SetUp/Map.cpp" "CMakeFiles/untitled.dir/SetUp/Map.cpp.o" "gcc" "CMakeFiles/untitled.dir/SetUp/Map.cpp.o.d"
+  "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/SetUp/Menu.cpp" "CMakeFiles/untitled.dir/SetUp/Menu.cpp.o" "gcc" "CMakeFiles/untitled.dir/SetUp/Menu.cpp.o.d"
+  "/home/francisca/uni/2ano/2S/DA/DA2425_PROJ1/SetUp/UFDS.cpp" "CMakeFiles/untitled.dir/SetUp/UFDS.cpp.o" "gcc" "CMakeFiles/untitled.dir/SetUp/UFDS.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
