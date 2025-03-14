0c1adbe5c24f0af78961a2869156b1e9288fbd9e
gab1mattos <gabydemattos@icloud.com>
2025-03-14 19:44:13 +0000

    Added things to menu and routes

--- SetUp/Menu.cpp
+++ SetUp/Menu.cpp
@@ -1,5 +1,7 @@
 #include "Menu.h"
 
+#include <sstream>
+
 
 int optionsMenu() {
 
@@ -21,4 +23,104 @@ int optionsMenu() {
         if (i > 4) cout << "Invalid input." << endl;
     } while (i > 4);
     return i;
+}
+
+template<class T>
+void optionBestRoute(Graph<T> *g) {
+    string mode;
+    int source, destination;
+
+    cout << "Mode: ";
+    cin >> mode;
+    if (!isModeDriving(mode))
+        cout << "Invalid input." << endl;
+
+    cout << "Source : ";
+    cin >> source;
+
+    cout << "Destination : ";
+    cin >> destination;
+
+    bestRoute(g->findVertex(source), g->findVertex(destination));
+}
+
+void optionRestrictedRoute() {
+    string mode;
+    int source, destination, includeNode;
+    vector<int> avoidNodes;
+    //vector<pair<int, int>> avoidSegments;
+
+    cout << "Mode: ";
+    cin >> mode;
+    if (!isModeDriving(mode))
+        cout << "Invalid input." << endl;
+
+    cout << "Source : ";
+    cin >> source;
+
+    cout << "Destination : ";
+    cin >> destination;
+
+    cout << "Enter nodes separated by spaces, then press ENTER. Enter -1 to skip" << endl;
+    cout << "AvoidNodes: ";
+    string input;
+    cin.ignore();
+    getline(cin, input);
+    stringstream ss(input);
+    int node;
+    while (ss >> node) {
+        if (node == -1) break;
+        avoidNodes.push_back(node);
+    }
+
+    cout << "AvoidSegments: ";
+    // to do
+
+    cout << "IncludeNode : ";
+    cin >> includeNode;
+
+}
+
+void optionEnvironmentalRoute() {
+    string mode;
+    int source, destination, maxWalkTime;
+    vector<int> avoidNodes;
+    //vector<pair<int, int>> avoidSegments;
+
+    cout << "Mode: ";
+    cin >> mode;
+    if (!isModeDriving(mode))
+        cout << "Invalid input." << endl;
+
+    cout << "Source : ";
+    cin >> source;
+
+    cout << "Destination : ";
+    cin >> destination;
+
+    cout << "IncludeNode : ";
+    cin >> maxWalkTime;
+
+    cout << "AvoidNodes: " << endl;
+    string input;
+    cin.ignore();
+    getline(cin, input);
+    stringstream ss(input);
+    int node;
+    while (ss >> node) {
+        if (node == -1) break;
+        avoidNodes.push_back(node);
+    }
+
+    cout << "AvoidSegments: " << endl;
+    // to do
+}
+
+
+// ???
+bool isModeDriving(const string& mode) {
+    string res;
+    for (const char c : mode)
+        res += static_cast<char>(tolower(c));
+    return res == "driving";
 }
