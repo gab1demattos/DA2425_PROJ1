2236efeccb00a05a02bedba12d46c69cf2b3a56b
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-03-20 18:54:39 +0000

    Added the csv files for Porto locations (much smaller graph) and fixed the segmentation fault

--- SetUp/Graph.h
+++ SetUp/Graph.h
@@ -147,6 +147,7 @@ public:
 
     //new!!
     Vertex<T> *findVertexByCode(const string &code) const;
+    void printGraph(const Graph<T>& graph);
 
     /*
      *  Adds a vertex with a given content or info (in) to a graph (this).
@@ -497,6 +498,30 @@ Vertex<T> * Graph<T>::findVertexByCode(const string &code) const {
         if (v->getCode() == code)
             return v;
     return nullptr;
+}
+
+template <class T>
+void printGraph(const Graph<T>& graph) {
+    std::cout << "Vertices:" << std::endl;
+    for (auto vertex : graph.getVertexSet()) {
+        std::cout << "ID: " << vertex->getInfo()
+                  << ", Code: " << vertex->getCode()
+                  << ", Parking: " << (vertex->isParkingAvailable() ? "Yes" : "No")
+                  << std::endl;
+    }
+
+    std::cout << "Edges:" << std::endl;
+    for (auto vertex : graph.getVertexSet()) {
+        for (auto edge : vertex->getAdj()) {
+            std::cout << "From: " << edge->getOrig()->getCode()
+                      << " (" << edge->getOrig()->getInfo() << ")"
+                      << " -> To: " << edge->getDest()->getCode()
+                      << " (" << edge->getDest()->getInfo() << ")"
+                      << ", Driving: " << edge->getDriving()
+                      << ", Walking: " << edge->getWalking()
+                      << std::endl;
+        }
+    }
 }
 //
 
